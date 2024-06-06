5a67b8db076b1cbd53977f91607a76f19218eaab
AntonNageh <92064286+AntonNageh@users.noreply.github.com>
2024-06-06 20:44:11 +0300

    Dialogues edited.

--- src/Constants.jsx
+++ src/Constants.jsx
@@ -2,22 +2,16 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
   pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+   Here is my Github <a href="https://github.com/AntonNageh" target="_blank">Github</a>!`,
+  "cs-degree": `This is my certificates. I hung it on the wall because I'm proud of it. They were great steps and I think they gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and play some games.`,
+  tv: `That's my TV. I've been watching a bunch of TV shows in my free time.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep.. :D`,
+  resume: `This is my desk and on it is my resume. <a href="https://mysvelteportfolio.vercel.app/assets/CV.pdf" target="_blank">Check it out?</a>
+  Contact me at anton_nageh20@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  Kaboom.js here : <a href="https://kaboomjs.com/guide/" target="_blank">Kaboom.js</a>`,
+  library: `I read novels and books in Psychology.`,
+  exit: `If you want to exit Anton Nageh's portfolio, just close the tab.`,
 };
